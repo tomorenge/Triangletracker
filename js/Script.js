fb95b981af9d7db65f5b60db24dd6fdfd111a9c9
tom orenge <tomorenge3@gmail.com>
2019-02-15 14:47:24 +0300

    added background photo to the triangletracker app

--- js/Script.js
+++ js/Script.js
@@ -1,18 +1,19 @@
+function findoutangle() {
 var hypotenuse = parseInt(prompt("hypotenuse side"));
 var opposite = parseInt(prompt("opposite side"));
 var adjacent = parseInt(prompt("adjacent side"));
 
 
-
-function findoutangle(hypotenuse,opposite,adjacent) {
-  if (hypotenuse===opposite&&opposite===adjacent&&hypotenuse===adjacent) {
+  if (hypotenuse === opposite && opposite === adjacent && hypotenuse === adjacent) {
     alert("Equilateral triangle");
   }
-    else if (hypotenuse===opposite||opposite===adjacent||hypotenuse==adjacent) {
+    else if (hypotenuse === opposite || opposite === adjacent || hypotenuse === adjacent) {
     alert("Isosceles triangle");
+  }
+  else if ((hypotenuse + opposite) <= adjacent ||(hypotenuse + adjacent) <= opposite ||(opposite + adjacent) <= hypotenuse) {
+    alert("Not a definite angle");
   }
     else {
     alert("scalene");
-
-}}
-  findoutangle(hypotenuse,opposite,adjacent)
+  }
+}
